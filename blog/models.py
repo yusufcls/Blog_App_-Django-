27edabb6d4aaaee62e54aa372ecab6f2540b64d1
Yusuf C <yusufcfly@gmail.com>
2022-09-05 20:25:31 +0200

    base_broke

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=200)
+    content = models.TextField(max_length=250)
+    # image = models.ImageField(upload_to)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    # last_updated = models.DateTimeField(auto_now=True)
+    # status = models.CharField(max_length=70)
+    
+    def __str__(self):
+        return f"{self.title}"
+
+
+
+
--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include("..templates/base.html"))
+    path ('', base, name="base"),
 ]
